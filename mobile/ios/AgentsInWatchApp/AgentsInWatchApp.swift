78a8fd7616ecb267c325b13609347e0ab33457bb
Robbie Qu <93100072+ZhenrongQu@users.noreply.github.com>
2026-06-16 10:05:02 -0700

    feat: add iPhone app target

---
+++ mobile/ios/AgentsInWatchApp/AgentsInWatchApp.swift
@@ -0,0 +1,11 @@
+import AgentsInWatchMobileUI
+import SwiftUI
+
+@main
+struct AgentsInWatchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            CompanionRootView()
+        }
+    }
+}
